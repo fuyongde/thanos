b2960f97e736a83b138e4f00e3705813adcada6e
fuyongde <fuyongde@zhuifintech.com>
2019-05-31 13:39:32 +0800

    feat: 采用nacos作为注册中心

--- gateway-demo/gateway2/src/main/java/com/marvel/thanos/gateway2/GatewayApplication2.java
+++ gateway-demo/gateway2/src/main/java/com/marvel/thanos/gateway2/GatewayApplication2.java
@@ -1,19 +1,22 @@
-package com.marvel.thanos.product.bootstrap;
+package com.marvel.thanos.gateway2;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author fuyongde
- * @date 2019/5/28
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ProductApplication {
+@EnableFeignClients
+@ServletComponentScan("com.marvel.thanos.gateway2")
+public class GatewayApplication2 {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(ProductApplication.class)
+        new SpringApplicationBuilder(GatewayApplication2.class)
                 .properties("spring.profiles.active=nacos")
                 .run(args);
     }
